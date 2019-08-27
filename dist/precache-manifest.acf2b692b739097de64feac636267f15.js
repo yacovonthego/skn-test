912e0cdcb9444c36fd9808506ee50855b5dad309
yakov <jugelca.nalpik@gmail.com>
2019-08-27 10:58:46 +0300

    added dist

---
+++ dist/precache-manifest.acf2b692b739097de64feac636267f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88696c4b10606a0fd181",
+    "url": "/css/app.1ab9f3b2.css"
+  },
+  {
+    "revision": "88696c4b10606a0fd181",
+    "url": "/js/app.f73110fe.js"
+  },
+  {
+    "revision": "86d529533e4e76f64794",
+    "url": "/js/chunk-vendors.c88340ba.js"
+  },
+  {
+    "revision": "309c105be245523ea69b2971ecdee6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
